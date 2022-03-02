e662dd7e7db7bc8f80a57b62803fe2940b94c523
c0basi <cobas050@uottawa.ca>
2022-03-02 15:48:39 -0500

    added the proct data

---
+++ server/models/Product.js
@@ -0,0 +1,14 @@
+class Product {
+	name;
+	description;
+	price;
+	countInStock;
+	imageUrl;
+	constructor(name, description, price, countInStock, imageUrl) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+}
+
+export default Product;
