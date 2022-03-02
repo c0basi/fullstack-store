2cd69d8582728d1191be44f56d18e3f2d0a75760
c0basi <cobas050@uottawa.ca>
2022-03-02 13:46:17 -0500

    changes

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 500;
+
+app.listen(3001, () => console.log(`server is runnin gon port ${port}`));
