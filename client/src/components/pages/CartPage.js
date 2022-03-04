f50fa67e80da92087ab094ebdc2d01044e436f96
c0basi <cobas050@uottawa.ca>
2022-03-03 21:59:45 -0500

    updated productPage

--- client/src/components/pages/CartPage.js
+++ client/src/components/pages/CartPage.js
@@ -1,7 +1,27 @@
 import './CartPage.scss';
+import CartItem from '../CartItem';
 
 const CartPage = () => {
-	return <div className="cartpage">CartPage</div>;
+	return (
+		<div className="cartpage">
+			<div className="cartpage__left">
+				<h2>Shopping Cart</h2>
+
+				<CartItem />
+			</div>
+			<div className="cartpage__right">
+				<div className="cartpage__right--info">
+					<p>Subtotal (0) items</p>
+					<p>$499.00</p>
+				</div>
+				<div>
+					<p>
+						<button>Proceed To Checkout</button>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CartPage;
